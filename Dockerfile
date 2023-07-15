3b04eb68647b01995c4802224f6b6185fa3b48e2
cFede11 <94903944+cFede11@users.noreply.github.com>
2023-07-16 00:54:29 +0900

    docstrings for main

--- Dockerfile
+++ Dockerfile
@@ -2,12 +2,15 @@ FROM python:3.9
 
 WORKDIR /app
 
+# Set the environment variable for the Alpha Vantage API key
 ENV AV_API_KEY="128S2A8V57C2W8UP"
 
 COPY requirements.txt .
 
 RUN pip install -r requirements.txt
 
+# Copy all files from the current directory to the working directory
 COPY . .
 
+# Expose port 5000 for the Flask application
 EXPOSE 5000
--- get_raw_data.py
+++ get_raw_data.py
@@ -1,35 +1,73 @@
+"""
+This module retrieves financial data from the Alpha Vantage API and upserts the data into a MySQL database.
+
+Module Dependencies:
+    - mysql.connector
+    - alpha_vantage.timeseries.TimeSeries
+    - os
+
+Environment Variables:
+    - AV_API_KEY: The API key for Alpha Vantage.
+
+MySQL Database Configuration:
+    - host (str): The hostname of the MySQL database.
+    - user (str): The username for the MySQL database.
+    - database (str): The name of the MySQL database.
+
+Symbols:
+    - symbols (list): The list of symbols for which to retrieve financial data.
+
+Exceptions:
+    - mysql.connector.Error: If there is an error with the MySQL connection.
+    - ValueError: If the AV_API_KEY environment variable is not set.
+    - Exception: For any other unexpected exceptions.
+"""
+
 import mysql.connector
 from alpha_vantage.timeseries import TimeSeries
 import os
 
 
-#MySQL database configuration
+# MySQL database configuration
 config = {
     'host': 'database',
     'user': f'root',
     'database': 'financial_db'
 }
 
-#get API KEY for Alpha Vantage
+# Get API KEY for Alpha Vantage
 api_key = os.environ.get('AV_API_KEY')
 
-#check if the API key is available
+# Check if the API key is available
 if api_key is None:
     raise ValueError('AV_API_KEY environment variable is not set')
 
-#initialize Alpha Vantage
+# Initialize Alpha Vantage
 ts = TimeSeries(key=api_key, output_format='pandas')
 
-#symbols to be used (META MSFT)
+# Symbols to be used (META MSFT)
 symbols = ['IBM', 'AAPL']
 
 try:
-    #connect to the MySQL database
+    # Connect to the MySQL database
     conn = mysql.connector.connect(**config)
     cursor = conn.cursor(dictionary=True)
 
     def upsert_stock_record(symbol, date, open_price, close_price, volume):
-        #insert and check for repeated values (upsert operation)
+        """
+        Upserts a stock record into the financial_data table of the MySQL database.
+
+        Args:
+            symbol (str): The stock symbol.
+            date (str): The date of the record.
+            open_price (float): The opening price.
+            close_price (float): The closing price.
+            volume (float): The volume.
+
+        Returns:
+            None
+        """
+        # Insert and check for repeated values (upsert operation)
         upsert_query = '''
             INSERT INTO financial_data (symbol, date, open_price, close_price, volume)
             VALUES (%s, %s, %s, %s, %s)
@@ -43,7 +81,7 @@ try:
 
     for symbol in symbols:
         data, meta_data = ts.get_daily_adjusted(symbol=symbol, outputsize='compact')
-        #get the last 14 days data
+        # Get the last 14 days data
         recent_data = data.tail(14)
 
         for date, values in recent_data.iterrows():
@@ -52,7 +90,7 @@ try:
             close_price = values['4. close']
             volume = values['6. volume']
 
-            #update the database
+            # Update the database
             upsert_stock_record(symbol, date, open_price, close_price, volume)
 
             stock_info = {
@@ -68,7 +106,7 @@ try:
     conn.close()
 
 except mysql.connector.Error as e:
-    #handle MySQL connection errors
+    # Handle MySQL connection errors
     error_code = e.errno
     error_msg = e.msg
     if error_code == 1045:
@@ -78,5 +116,5 @@ except mysql.connector.Error as e:
     else:
         print(f"Error: {error_msg}")
 except Exception as e:
-    #handle other exceptions
+    # Handle other exceptions
     print(f"Error: {str(e)}")
